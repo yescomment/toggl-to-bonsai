995fde976fcc569b47ce9768a1229bbb99be504d
Jacob Ford <j@cobford.com>
2021-08-22 14:24:23 -0400

    Add Date & Time cols

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const parse = require('csv-parse')
 const fs = require('fs')
 const { finished } = require('stream/promises')
 const { timeStrToDecimal } = require('pretty-time-decimal')
+const convertTime = require('convert-time')
 
 const inputFilename = 'togglExportExample.csv'
 const outputFilename = 'bonsaihours.csv'
@@ -16,6 +17,11 @@ const processFile = async () => {
 
     for await (const row of parser) {
       row['Hours'] = timeStrToDecimal(row['Duration'])
+      row['Hours'] = Math.ceil(row['Hours'] * 10) / 10 // round up to nearest 6 mins
+
+      row['Date'] = row['Start date']
+      row['Time'] = convertTime(row['Start time'], 'hh:MM A')
+
       rows.push(row)
     }
 
